9b75a75331c663aa4b13aadf9813fbbb042ffe58
OmarBiH7 <omarfx7@hotmail.com>
2025-11-25 22:31:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Welcome to Rock, Paper, Scissors!");
+
+function getComputerChoice(rock, paper, Scissors) {
+  return Math.floor(Math.random() * 3);
+}
+console.log(getComputerChoice(3));
