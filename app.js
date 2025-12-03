97822b859175dbf37bcb775e9309b1f7951a994e
OmarBiH7 <omarfx7@hotmail.com>
2025-12-03 20:57:39 +0100

    playround

--- app.js
+++ app.js
@@ -16,11 +16,35 @@ console.log(getComputerChoice(3));
 
 function getHumanChoice() {
   let Humanchoice = prompt("Enter Rock, Paper or Sciccors");
-
-  let humanScore = 0;
-  let computerScore = 0;
 }
 console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return "it's a draw!";
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+      return "You lose! Paper beats Rock";
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+      return "You Win! Rock beats Scissors";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+      return "you win! Paper beats Rock";
+    } else if (humanChoice === "Paper" && computerChoice === "Sciccors") {
+      return "You lose! Sciccors beats Paper";
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+      return "You lose! Rock beats Sciccors";
+    } else humanChoice === "Sciccors" && computerChoice === "Paper";
+    return "You win! Scoccors beats Rock";
+  }
+
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let playRound = 0; playRound < 5; playRound++) {}
+}
+console.log(computerChoice(playRound));
