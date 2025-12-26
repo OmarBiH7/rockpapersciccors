7535372868583e52a43b55a33696b5e757d40502
OmarBiH7 <omarfx7@hotmail.com>
2025-12-26 18:19:36 +0100

    cpmputerchoice fix

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-console.log("Welcome to Rock, Paper, Scissors!");
+console.log("Welcome to Rock, Paper, Sciccors!");
 
-function getComputerChoice() {
+function getComputerChoice(Rock, Paper, sciccors) {
   const randomNumber = Math.floor(Math.random() * 3);
 
   if (randomNumber === 1) {
     return "Rock";
   } else if (randomNumber === 2) {
     return "Paper";
-  } else {
+  } else randomNumber === 3;
+  {
     return "Sciccors";
   }
 }
@@ -16,38 +17,38 @@ console.log(getComputerChoice(3));
 
 function getHumanChoice() {
   let Humanchoice = prompt("Enter Rock, Paper or Sciccors");
+  return Humanchoice;
 }
-console.log(getHumanChoice());
+console.log(getHumanChoice(3));
 
-function playGame() {
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      return "it's a draw!";
-    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-      return "You lose! Paper beats Rock";
-    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-      return "You Win! Rock beats Scissors";
-    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-      return "you win! Paper beats Rock";
-    } else if (humanChoice === "Paper" && computerChoice === "Sciccors") {
-      return "You lose! Sciccors beats Paper";
-    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-      return "You lose! Rock beats Sciccors";
-    } else humanChoice === "Sciccors" && computerChoice === "Paper";
-    return "You win! Scoccors beats Rock";
+function playGame() {}
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "it's a draw!";
+  } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+    return "You lose!  Paper beats Rock";
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    return "You Win! Rock beats Sciccors";
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    return "you win! Paper beats Rock";
+  } else if (humanChoice === "Paper" && computerChoice === "Sciccors") {
+    return "You lose! Sciccors beats Paper";
+  } else if (humanChoice === "Sciccors" && computerChoice === "Rock") {
+    return "You lose! Rock beats Sciccors";
+  } else humanChoice === "Sciccors" && computerChoice === "Paper";
+  return "You win! Sciccors beats Paper";
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
 
-    let humanScore = 0;
-    let computerScore = 0;
-  }
+  let humanScore = 0;
+  let computerScore = 0;
 }
 
-console.log(playRound)(getHumanChoice(), getComputerChoice());
-console.log(playRound)(getHumanChoice(), getComputerChoice());
-console.log(playRound)(getHumanChoice(), getComputerChoice());
-console.log(playRound)(getHumanChoice(), getComputerChoice());
-console.log(playRound)(getHumanChoice(), getComputerChoice());
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound(getHumanChoice(), getComputerChoice()));
