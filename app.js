08dd34150734f8b592503248ab50d407185846d2
OmarBiH7 <omarfx7@hotmail.com>
2026-01-05 20:51:12 +0100

    refactor: streamline playGame and playRound functions for clarity

--- app.js
+++ app.js
@@ -21,34 +21,24 @@ function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  console.log("score - you: ${humanScore} Computer: ${computerScore}");
-}
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return "it's a draw!";
-  } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-    return "You lose!  Paper beats Rock";
-  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    return "You Win! Rock beats Sciccors";
-  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    return "you win! Paper beats Rock";
-  } else if (humanChoice === "Paper" && computerChoice === "Sciccors") {
-    return "You lose! Sciccors beats Paper";
-  } else if (humanChoice === "Sciccors" && computerChoice === "Rock") {
-    return "You lose! Rock beats Sciccors";
-  } else humanChoice === "Sciccors" && computerChoice === "Paper";
-  return "You win! Sciccors beats Paper";
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return console.log("it's a draw!");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "sciccors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "sciccors" && computerChoice === "paper")
+    ) {
+      return console.log(`You Win! ${humanChoice} beats ${computerScore} `);
+    } else {
+      return console.log(`You Win! ${computerChoice} beats ${humanChoice} `);
+    }
+  }
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
 }
 
-console.log("score - you: ${humanScore} Computer: ${computerScore}");
-
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+playGame();
